6971e82711052a941572c23f35656249b5ad5804
Yash Patel <ypatel5400@gmail.com>
2018-12-02 10:46:20 -0800

    Auto-redirect (really ratchet workaround)

--- frontend/demos/demos.js
+++ frontend/demos/demos.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export class Demos extends React.Component {
   render() {
+    window.location.assign('/demos/demo4');
     return (
       <div>
-        <div className='demo-link'><a onClick={() => window.location.assign('/demos/demo4')}>Demo 4</a></div>
       </div>
     );
   }
