4515aa049630c4a295b37b5dfc42b71a55a8aaf6
Yash Patel <ypatel5400@gmail.com>
2018-12-02 10:49:36 -0800

    Made default start

--- frontend/demos/index.js
+++ frontend/demos/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Demos } from './demos';
 import { AppContainer } from 'react-hot-loader';
-import './test.scss';
+import { Provider } from 'react-redux';
+import { store } from './demo4/store';
+import { Demo4 } from './demo4/demo4';
 
 window.onload = () => {
   const rootEl = document.getElementById('root');
-  const render = Component =>
+  const render = Component => {
     ReactDOM.render(
-      <AppContainer>
-        <Component />
-      </AppContainer>,
+      <Provider store={store}>
+        <AppContainer>
+          <Component />
+        </AppContainer>
+      </Provider>,
       rootEl
     );
+  };
 
-  render(Demos);
-  if (module.hot) module.hot.accept('./demos', () => render(Demos));
+  render(Demo4);
+  if (module.hot) module.hot.accept('./demo4/demo4', () => render(Demo4));
 };
