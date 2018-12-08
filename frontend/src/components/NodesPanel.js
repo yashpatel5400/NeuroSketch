f5041e64fed7c52fdd23ddb484f42a668159b07d
Yash Patel <ypatel5400@gmail.com>
2018-12-08 11:53:00 -0800

    Added all layers (complete mess)

--- frontend/src/components/NodesPanel.js
+++ frontend/src/components/NodesPanel.js
@@ -37,6 +37,144 @@ export class NodesPanel extends React.Component {
   render() {
     const { selectedNode, onShow, onUndo, onRedo, canUndo, canRedo } = this.props;
 
+    const layersTypesToNodes = {
+      "Core" : [
+        "Dense",
+        "Activation",
+        "Dropout",
+        "Flatten",
+        "Input",
+        "Reshape",
+        "Permute",
+        "RepeatVector",
+        "Lambda",
+        "ActivityRegularization",
+        "Masking",
+        "SpatialDropout1D",
+        "SpatialDropout2D",
+        "SpatialDropout3D"
+      ],
+
+      "Convolutional" : [
+        "Conv1D",
+        "Conv2D",
+        "Conv3D",
+        "SeparableConv1D",
+        "SeparableConv2D",
+        "DepthwiseConv2D",
+        "Conv2DTranspose",
+        "Conv3DTranspose",
+        "Cropping1D",
+        "Cropping2D",
+        "Cropping3D",
+        "UpSampling1D",
+        "UpSampling2D",
+        "UpSampling3D",
+        "ZeroPadding1D",
+        "ZeroPadding2D",
+        "ZeroPadding3D"
+      ],
+
+      "Pooling" : [
+        "MaxPooling1D",
+        "MaxPooling2D",
+        "MaxPooling3D",
+        "AveragePooling1D",
+        "AveragePooling2D",
+        "AveragePooling3D",
+        "GlobalMaxPooling1D",
+        "GlobalMaxPooling2D",
+        "GlobalMaxPooling3D",
+        "GlobalAveragePooling1D",
+        "GlobalAveragePooling2D",
+        "GlobalAveragePooling3D"
+      ],
+
+      "Locally-Connected" : [
+        "LocallyConnected1D",
+        "LocallyConnected2D"
+      ],
+
+      "Recurrent" : [
+        "RNN",
+        "SimpleRNN",
+        "SimpleRNNCell",
+        "GRU",
+        "GRUCell",
+        "LSTM",
+        "LSTMCell",
+        "ConvLSTM2D",
+        "CuDNNGRU",
+        "CuDNNLSTM"
+      ],
+
+      "Embedding" : [
+        "Embedding"
+      ],
+
+      "Merge" : [
+        "Add",
+        "Subtract",
+        "Multiply",
+        "Average",
+        "Maximum",
+        "Concatenate",
+        "Dot"
+      ],
+
+      "Activation" : [
+        "LeakyReLU",
+        "PReLU",
+        "ReLU",
+        "ELU",
+        "ThresholdedReLU",
+        "Softmax"
+      ],
+
+      "Normalization" : [
+        "BatchNormalization"
+      ],
+
+      "Noise" : [
+        "GaussianNoise",
+        "GaussianDropout",
+        "AlphaDropout"
+      ]
+    }
+
+    var layerTypeToColor = {
+      "Core" : "danger",
+      "Convolutional" : "warning",
+      "Pooling" : "success",
+      "Locally-Connected" : "info",
+      "Recurrent" : "default",
+      "Embedding" : "primary",
+      "Merge" : "secondary",
+      "Activation" : "elegant",
+      "Normalization" : "stylish",
+      "Noise" : "unique"
+    }
+
+    var layerTypes = Object.keys(layersTypesToNodes);
+    var panelCards = [];
+    for (var i = 0; i < layerTypes.length; i++) {
+      var layerType = layerTypes[i];
+      var layerColor = layerTypeToColor[layerType];
+      var headerColor = layerColor + "-color";
+      var kerasNodes = layersTypesToNodes[layerType];
+      var nodes = [];
+      for (var j = 0; j < kerasNodes.length; j++) {
+        nodes.push(<Node type='connection' color='#007E33' name={ kerasNodes[j] } />);
+      }
+
+      panelCards.push(
+        <MDBCard color="special-color" border={layerColor} style={{ width: "22rem", marginTop: "1rem" }} className="text-center">
+          <MDBCardHeader color={headerColor}>{ layerType } Layers</MDBCardHeader>
+          <MDBCardBody> {nodes} </MDBCardBody>
+        </MDBCard>
+      );
+    }    
+
     return (
       <div className='nodes-panel'>
         <NavbarBrand>
@@ -55,21 +193,8 @@ export class NodesPanel extends React.Component {
           <Node type='output' name='Data Node' />
         </div>
 
-        <MDBCard color="special-color" border="success" style={{ width: "22rem", marginTop: "1rem" }} className="text-center">
-          <MDBCardHeader color="success-color">Core Layers</MDBCardHeader>
-          <MDBCardBody color="success-color">
-            <div className='node-wrapper'>
-              <Node type='connection' color='rgb(224, 98, 20)' name='Dense Node' />
-            </div>
-            <div className='node-wrapper'>
-              <Node type='connection' color='rgb(157, 13, 193)' name='Conv2D Node' />
-            </div>
-            <div className='node-wrapper'>
-              <Node type='connection' color='rgb(12, 193, 180)' name='LSTM Node' />
-            </div>
-          </MDBCardBody>
-        </MDBCard>
-        
+        { panelCards }
+
         <div className='node-wrapper'>
           <Node type='input' name='Output Node' />
         </div>
