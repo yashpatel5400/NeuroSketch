a8e28325edc761516080a471469d8d51c244b789
Yash Patel <ypatel5400@gmail.com>
2018-12-15 11:04:52 -0800

    Added build script

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,12 @@
-#----------------------------------------------------------------------------#
-# Imports
-#----------------------------------------------------------------------------#
-
 from flask import Flask, render_template, request
 import os
-
-#----------------------------------------------------------------------------#
-# App Config.
-#----------------------------------------------------------------------------#
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-#----------------------------------------------------------------------------#
-# Controllers.
-#----------------------------------------------------------------------------#
-
-
 @app.route('/')
 def home():
-    return render_template('main.html')
+    return render_template('index.html')
 
-
-@app.errorhandler(500)
-def internal_error(error):
-    #db_session.rollback()
-    return render_template('errors/500.html'), 500
-
-
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('errors/404.html'), 404
-
-#----------------------------------------------------------------------------#
-# Launch.
-#----------------------------------------------------------------------------#
-
-# Default port:
 if __name__ == '__main__':
     app.run()
---
+++ build.sh
@@ -0,0 +1,17 @@
+cd frontend/
+
+npm run build
+
+rm -rf ../backend/build
+
+mv build ../backend/
+
+cd ../backend/
+
+mv build/static static/
+
+mv build/index.html templates/
+
+rm -rf build/
+
+python3 app.py
