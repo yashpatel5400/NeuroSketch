0a00ee121c0179ec9cd0768474b742d76b933e13
Yash Patel <ypatel5400@gmail.com>
2018-12-15 11:05:10 -0800

    Formatted build

--- build.sh
+++ build.sh
@@ -1,17 +1,9 @@
 cd frontend/
-
 npm run build
-
 rm -rf ../backend/build
-
 mv build ../backend/
-
 cd ../backend/
-
 mv build/static static/
-
 mv build/index.html templates/
-
 rm -rf build/
-
 python3 app.py
