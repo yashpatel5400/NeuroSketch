e5561815b2842efb60aee3ee14a850ec81efd1ac
Yash Patel <ypatel5400@gmail.com>
2018-12-16 13:16:33 -0800

    Added Keras conversion

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 import json
 import networkx as nx
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+
+import keras.optimizers
+from keras.models import Sequential
+
+from lookup import name_to_layer
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,6 +14,14 @@ app.config.from_object('config')
 def home():
     return render_template('index.html')
 
+def convert(model, df):
+    model.save("./model.h5")
+    command = """mmconvert \
+        --srcFramework keras \
+        --inputWeight ./model.h5 \
+        --dstFramework {} \
+        --outputModel ./model_{}""".format(df, df)
+    subprocess.Popen(command, shell=True)
 
 @app.route('/compile', methods=['GET','POST'])
 def compile():
@@ -26,6 +36,22 @@ def compile():
     for link in raw_graph['edges']:
         src, dst = link
         G.add_edge(src, dst)
+
+    # seqential ordering of nodes is its topological sort
+    sequential_order = list(nx.topological_sort(G))
+    sequential_layers = [G.node[node_id]["name"] for node_id in sequential_order]
+    
+    model = Sequential()
+
+    for layer in sequential_layers:
+        model.add(name_to_layer[layer]())
+
+    # standard optimizer and loss function (assuming categorical data)
+    opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=opt,
+                  metrics=['accuracy'])
+    convert(model, "tensorflow")
 
     # draws the graph (for debugging purposes)
     # nx.draw_networkx(G)
